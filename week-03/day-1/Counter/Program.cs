794171eba5d0de0764d3da2dd91bef9249af68e7
Michal Adamec <tritis@seznam.cz>
2021-09-20 17:49:24 +0200

    exercise from 20.9.

---
+++ week-03/day-1/Counter/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+//Create Counter class
+//which has a counter (integer) field
+//at creation it should have a default value 0
+//there are two ways to create the class:
+//with 0 input parameters
+//with 1 input parameter called counter which is set as the initial value of the counter field
+//we can add another whole number to this counter: Add(number)
+//we can just increase the counter's value by one: Add() (no parameters)
+//we can Get() the current counter value
+//and we can Reset() the counter to the initial value set during the construction of the class
+namespace Counter
+{
+    public class Counter
+    {
+        private int CounteR = 0;
+
+        public Counter()
+        {
+        }
+        public void Add()
+        { 
+            CounteR++;
+        }
+        public int Get()
+        { return CounteR; }
+        public void Reset()
+        { CounteR = 0; }
+
+        public void Print()
+        { Console.WriteLine($"Counter is: {CounteR}"); }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter counter = new Counter();
+            counter.Add();
+            counter.Add();
+            counter.Get();
+            counter.Reset();
+            counter.Print();
+        }
+    }
+}
---
+++ week-03/day-1/Pokemon/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Every pokemon has a name and a type.
+// Certain types are effective against others, e.g. water is effective against fire.
+// Ash has a few pokemon.
+// A wild pokemon appeared!
+// Which pokemon should Ash use?
+
+namespace GreenFoxAcademy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Pokemon> pokemonOfAsh = InitializePokemon();
+
+            Pokemon wildPokemon = new Pokemon("Oddish", "grass", "water");
+
+            int lengthOfList = pokemonOfAsh.Count;
+            for (int a = 0; a < lengthOfList; a++)
+            {
+                //if (pokemonOfAsh[a].IsEffectiveAgainst(wildPokemon))
+                if (pokemonOfAsh[a].EffectiveAgainst == wildPokemon.Type)
+                {
+                    Console.Write($"I choose you, {pokemonOfAsh[a].Name}!");
+                }
+            }
+        }
+        private static List<Pokemon> InitializePokemon()
+        {
+            return new List<Pokemon>
+            {
+                new Pokemon("Bulbasaur", "grass", "water"),
+                new Pokemon("Pikachu", "electric", "water"),
+                new Pokemon("Charizard", "fire", "grass"),
+                new Pokemon("Pidgeot", "flying", "fighting"),
+                new Pokemon("Kingler", "water", "fire")
+            };
+        }
+    }
+}
