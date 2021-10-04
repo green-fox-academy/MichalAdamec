f5c7dd24b4f01d0b7cdeb06d65648b8133ef5c30
Michal Adamec <tritis@seznam.cz>
2021-10-04 17:50:15 +0200

    added some Hard exercises from 1st week

---
+++ week-01/day-5/Subint/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+//  Create a function that takes a number and an array of integers as parameters
+//  It returns the indices of the integers of the array which contain the given number
+//  or returns an empty array (if the number is not part of any of the integers in the array)
+//  Example:
+//Console.WriteLine(FindMatchingIndexes(1, new int[] { 1, 11, 34, 52, 61 }));
+//  should print: `[0, 1, 4]`
+//Console.WriteLine(FindMatchingIndexes(9, new int[] { 1, 11, 34, 52, 61 }));
+//  should print: '[]'
+
+namespace Subint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 4;
+            int[] array = new int[] { 4, 2, 14, 25, 13, 29, 8, 45, 42, 48, 81 };
+
+            FindMatchingIndexes(n, array);
+        }
+        public static void FindMatchingIndexes(int input, int[] array)
+        {
+            int[] result = new int[array.Length];
+
+            Console.Write("[");
+            for (int a = 0; a < array.Length; a++)
+            {
+                if ((array[a] % 10) == input || (array[a] / 10) == input)
+                {
+                    result[a] = a;
+                }
+                else
+                    result[a] = 99;
+            }
+            foreach (int index in result)
+            {
+                if (index != 99)
+                {
+                    Console.Write(index + " ");
+                }
+
+            }
+            Console.Write("]");
+        }
+    }
+}
---
+++ week-01/day-5/Uniques/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+//  Create a method that takes an array of integers as a parameter
+//  and returns an array of integers where every integer is unique (occurs only once)
+//  Example
+//Console.WriteLine(FindUniqueItems(new[] { 1, 11, 34, 11, 52, 61, 1, 34 }));
+//  should print: `[1, 11, 34, 52, 61]`
+
+namespace Uniques
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 1, 11, 34, 11, 52, 61, 1, 34 };       //[1, 11, 34, 52, 61]
+            int[] uniqueArray = FindUniqueItems(array);
+            foreach (int numb in uniqueArray)
+            {
+                Console.Write(numb + " " );
+            }
+        }
+        public static int[] FindUniqueItems(int[] array)
+        {
+            int[] uniqueArray = new int[array.Length];
+            for(int a = 0; a < array.Length; a++)
+            {
+                for(int b = 1; b < array.Length; b++)
+                {
+                    if (array[a] == array[b])
+                    {
+                        uniqueArray[a] = array[a];
+                    }
+                    else
+                        uniqueArray[a] = 0;
+                }
+            }
+            return uniqueArray;
+        }
+
+    }
+}
